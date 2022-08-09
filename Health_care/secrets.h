3ac9714baca85a622933faf421abf14f7fbfac5c
MYTHEESH C <91107867+mc-08@users.noreply.github.com>
2022-08-09 14:15:03 +0530

    Add files via upload

---
+++ Health_care/secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "Mytheesh"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "mytheesh1626" // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1769161     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "HBIHN9SFHBAMJLS1"   // replace XYZ with your channel write API Key
